6317980ad41c56f60fa6a1a05904a39d95eec68c
moeka802 <moeka802.redapple@gmail.com>
2020-11-01 22:42:08 +0900

    todolistを作成

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav
+    class="navbar is-fixed-top is-primary"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <h1 class="navbar-item">BULMA Todo List</h1>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+</style>
